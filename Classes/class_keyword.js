eda82f02ac3e284c96454c07da7b0ba6e0fba8a1
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-06-27 13:03:28 +0100

    Classes and instances

---
+++ Classes/class_keyword.js
@@ -0,0 +1,10 @@
+class Student {
+    constructor(firstName, lastName, year) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = year;
+    }
+}
+
+let firstStudent = new Student("Nikunj", "Raghav", 1);
+let secondStudent = new Student("Diya", "Raghav", 2);
